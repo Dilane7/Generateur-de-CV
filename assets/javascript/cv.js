4de22ded82d226a984781ba4662f5778ac2a7f23
Dilane <tsaguedilane7@gmail.com>
2025-02-25 23:11:26 +0100

    ajout de la page d'acceuil

--- assets/javascript/cv.js
+++ assets/javascript/cv.js
@@ -44,4 +44,191 @@ imageInput.addEventListener('change', (event) => {
     imagePreview.src = '#'; // Réinitialiser l'image si aucun fichier n'est sélectionné
   }
 });
-      
+
+
+// const competencesContainer = document.getElementById('competences-container');
+// const addCompetenceButton = document.getElementById('add-competence');
+
+// addCompetenceButton.addEventListener('click', () => {
+//   const newForm = document.createElement('div');
+//   newForm.classList.add('competence-form', 'border', 'p-4', 'rounded', 'mt-4'); // Ajoute une marge en haut
+
+//   newForm.innerHTML = `
+//     <input type="text" name="competence" placeholder="Compétence" class="border rounded p-2">
+//     <select name="niveau" class="border rounded p-2">
+//       <option value="debutant">Débutant</option>
+//       <option value="intermediaire">Intermédiaire</option>
+//       <option value="expert">Expert</option>
+//     </select>
+//     <button class="remove-competence bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-2">
+//       Supprimer
+//     </button>
+//   `;
+
+//   competencesContainer.appendChild(newForm);
+
+//   // Ajouter un gestionnaire d'événements pour le bouton "Supprimer"
+//   const removeButton = newForm.querySelector('.remove-competence');
+//   removeButton.addEventListener('click', () => {
+//     competencesContainer.removeChild(newForm);
+//   });
+// });
+
+const nom = document.getElementById('nom');
+const text_nom = document.getElementById('text_nom');
+const titre = document.getElementById('titre');
+const titre_text = document.getElementById('titre_text');
+const description_profil = document.getElementById('description_profil');
+const content_profil_description = document.getElementById('content_profil_description');
+const image = document.getElementById('imagePreview');
+const image_cv = document.getElementById('image_cv');
+const btn_importer = document.getElementById('btn_importer');
+
+
+const formation_name = document.getElementById('formation');
+const content_formation_name = document.getElementById('content_formation_name');
+const debut_formation = document.getElementById('debut_formation');
+const content_debut_formation = document.getElementById('content_debut_formation');
+const fin_formation = document.getElementById('fin_formation');
+const content_fin_formation = document.getElementById('content_fin_formation');
+const etablisement = document.getElementById('etablisement');
+const content_etablisement = document.getElementById('content_etablisement');
+const ville_formation  = document.getElementById('ville_formation ');
+const content_ville_formation = document.getElementById('content_ville_formation');
+const for_description = document.getElementById('for_description');
+const content_for_description = document.getElementById('content_for_description');
+
+const exp_name = document.getElementById('exp_name');
+const content_exp_name = document.getElementById('content_exp_name');
+const debut_exp = document.getElementById('debut_exp');
+const content_debut_exp = document.getElementById('content_debut_exp');
+const fin_exp = document.getElementById('fin_exp');
+const content_fin_exp = document.getElementById('content_fin_exp');
+const employeur = document.getElementById('employeur');
+const content_employeur = document.getElementById('content_employeur');
+const ville_exp  = document.getElementById('ville_exp');
+const content_ville_exp = document.getElementById('content_ville_exp');
+const exp_description = document.getElementById('exp_description');
+const content_exp_description = document.getElementById('content_exp_description');
+
+const ref_name = document.getElementById('ref_name');
+const content_ref_name = document.getElementById('content_ref_name');
+const ref_emp = document.getElementById('ref_emp');
+const content_ref_emp = document.getElementById('content_ref_emp');
+const ref_ville = document.getElementById('ref_ville');
+const content_ref_ville = document.getElementById('content_ref_ville');
+const num_ref = document.getElementById('num_ref');
+const content_num_ref = document.getElementById('content_num_ref');
+const email_ref = document.getElementById('email_ref');
+const content_email_ref = document.getElementById('content_email_ref');
+
+
+nom.addEventListener('input', function(){
+  const valeur_nom = nom.value;
+  text_nom.textContent = valeur_nom;
+});
+
+
+titre.addEventListener('input', function(){
+  const valeur_titre = titre.value;
+  titre_text.textContent = valeur_titre;
+})
+
+description_profil.addEventListener('input', function(){
+  const valeur_description_profil = description_profil.value;
+  content_profil_description.textContent = valeur_description_profil;
+})
+
+image.addEventListener('change' , function(){
+  const src_preview = image.src;
+  const src_image_cv = image.src;
+  src_image_cv = src_preview;
+});
+
+formation_name.addEventListener('input', function(){
+  const valeur_formation_name = formation_name.value;
+  content_formation_name.textContent = valeur_formation_name;
+});
+
+debut_formation.addEventListener('input', function(){
+  const valeur_debut_formation = debut_formation.value;
+  content_debut_formation.textContent = `du ${valeur_debut_formation} `;
+});
+
+fin_formation.addEventListener('input', function(){
+  const valeur_fin_formation = fin_formation.value;
+  content_fin_formation.textContent = ` au ${valeur_fin_formation}`;
+});
+
+etablisement.addEventListener('input', function(){
+  const valeur_etablisement = etablisement.value;
+  content_etablisement.textContent = `${valeur_etablisement}, `;
+});
+
+ville_formation.addEventListener('input', function(){
+  const valeur_ville_formation = ville_formation.value;
+  content_ville_formation.textContent = `${valeur_ville_formation}`;
+});
+
+for_description.addEventListener('input', function(){
+  const valeur_for_description = for_description.value;
+  content_for_description.textContent = `${valeur_for_description}`;
+}); 
+
+// experience
+exp_name.addEventListener('input', function(){
+  const valeur_exp_name = exp_name.value;
+  content_exp_name.textContent = valeur_exp_name;
+});
+
+debut_exp.addEventListener('input', function(){
+  const valeur_debut_exp = debut_exp.value;
+  content_debut_exp.textContent = `du ${valeur_debut_exp} `;
+});
+
+fin_exp.addEventListener('input', function(){
+  const valeur_fin_exp = fin_exp.value;
+  content_fin_exp.textContent = ` au ${valeur_fin_exp}`;
+});
+
+employeur.addEventListener('input', function(){
+  const valeur_employeur = employeur.value;
+  content_employeur.textContent = `${valeur_employeur}, `;
+});
+
+ville_exp.addEventListener('input', function(){
+  const valeur_ville_exp = ville_exp.value;
+  content_ville_exp.textContent = `${valeur_ville_exp}`;
+});
+
+exp_description.addEventListener('input', function(){
+  const valeur_exp_description = exp_description.value;
+  content_exp_description.textContent = `${valeur_exp_description}`;
+}); 
+
+// reference
+
+ref_name.addEventListener('input', function(){
+  const valeur_ref_name = ref_name.value;
+  content_ref_name.textContent = valeur_ref_name;
+});
+
+ref_emp.addEventListener('input', function(){
+  const valeur_ref_emp = ref_emp.value;
+  content_ref_emp.textContent = `${valeur_ref_emp}, `;
+});
+
+ref_ville.addEventListener('input', function(){
+  const valeur_ref_ville = ref_ville.value;
+  content_ref_ville.textContent = `${valeur_ref_ville}`;
+});
+
+num_ref.addEventListener('input', function(){
+  const valeur_num_ref = num_ref.value;
+  content_num_ref.textContent = `${valeur_num_ref}, `;
+});
+
+email_ref.addEventListener('input', function(){
+  const valeur_email_ref = email_ref.value;
+  content_email_ref.textContent = `${valeur_email_ref}`;
+});
